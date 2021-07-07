b73b87d948ba07bab054596d05edbee616acb609
madhukarp79 <madhukarp.79@gmail.com>
2021-07-07 05:58:55 +0200

    first commit

---
+++ Url.js
@@ -0,0 +1,10 @@
+var http=require('http');
+var url=require('url');
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    //res.write("The current date and time is :"+dt.mydateTime());
+    //var sample=url.URLSearchParams(req.url,true).query;
+    var sample=url.parse(req.url,true).query;
+    var output=sample.year +" "+ sample.name;
+    res.end(output);
+}).listen(8082);
